9cd6d57296576a3f9fd4d8ea17ec878bc3efdc6a
Dongliang Li <dongliang14@gmail.com>
2023-04-10 12:55:15 -0700

    divie into 3 sections

---
+++ react-app/src/components/MainPage/index.js
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+import LeftPanel from "../LeftPanel";
+import TaskList from "../TaskList";
+import RightPanel from "../RightPanel";
+
+function MainPage() {
+  return (
+    <div className="flx mrg20p">
+      <LeftPanel />
+      <TaskList />
+      <RightPanel />
+    </div>
+  )
+}
+
+export default MainPage;
---
+++ react-app/src/components/RightPanel/index.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+
+function RightPanel() {
+  return (
+    <div>
+      <div>use ternary to render either one depends on props</div>
+      <div>list summary</div>
+      <div>task detail</div>
+    </div>
+  )
+}
+
+export default RightPanel;
